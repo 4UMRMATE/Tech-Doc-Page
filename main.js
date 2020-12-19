1855b4920bc831890d2cb58da4856cba18593baf
4UMRMATE <sich.mate@gmail.com>
2020-12-20 02:19:42 +0400

    --init

---
+++ main.js
@@ -0,0 +1,22 @@
+const navbar = document.querySelector("#navbar");
+const navLinks = document.querySelectorAll(".nav-link");
+const mainDoc = document.querySelector("#main-doc");
+let navbarHidden = true;
+
+function showNavbar() {
+  if (navbarHidden) {
+    navbar.style.cssText = "display: flex";
+    mainDoc.style.cssText = "display: none";
+    navbarHidden = false;
+  } else {
+    navbar.style.cssText = "display: none";
+    mainDoc.style.cssText = "display: block";
+    navbarHidden = true;
+  }
+}
+
+navLinks.forEach(function (elem) {
+  elem.addEventListener("click", function () {
+    showNavbar();
+  });
+});
